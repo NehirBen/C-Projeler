ab2c2d0f56d0a3ea2481d6b447c213259073b2ff
Nehir <87086609+NehirBen@users.noreply.github.com>
2021-07-11 11:05:06 +0300

    Add files via upload

---
+++ Zar Oyunu/Zar Oyunu/Fr_Zar.Designer.cs
@@ -0,0 +1,212 @@
+﻿
+namespace Zar_Oyunu
+{
+    partial class Fr_Zar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btt_yndn = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lbl_2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lbl_1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btt_2 = new System.Windows.Forms.Button();
+            this.btt_1 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_btt_kctbtsn = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btt_yndn
+            // 
+            this.btt_yndn.Location = new System.Drawing.Point(106, 370);
+            this.btt_yndn.Name = "btt_yndn";
+            this.btt_yndn.Size = new System.Drawing.Size(105, 23);
+            this.btt_yndn.TabIndex = 20;
+            this.btt_yndn.Text = "Yeniden Başla";
+            this.btt_yndn.UseVisualStyleBackColor = true;
+            this.btt_yndn.Click += new System.EventHandler(this.btt_yndn_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(133, 344);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "label4";
+            // 
+            // lbl_2
+            // 
+            this.lbl_2.AutoSize = true;
+            this.lbl_2.Location = new System.Drawing.Point(261, 280);
+            this.lbl_2.Name = "lbl_2";
+            this.lbl_2.Size = new System.Drawing.Size(13, 13);
+            this.lbl_2.TabIndex = 15;
+            this.lbl_2.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(182, 280);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(73, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Toplam Puan:";
+            // 
+            // lbl_1
+            // 
+            this.lbl_1.AutoSize = true;
+            this.lbl_1.Location = new System.Drawing.Point(103, 280);
+            this.lbl_1.Name = "lbl_1";
+            this.lbl_1.Size = new System.Drawing.Size(13, 13);
+            this.lbl_1.TabIndex = 17;
+            this.lbl_1.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 280);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 13);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Toplam Puan:";
+            // 
+            // btt_2
+            // 
+            this.btt_2.Location = new System.Drawing.Point(185, 221);
+            this.btt_2.Name = "btt_2";
+            this.btt_2.Size = new System.Drawing.Size(128, 35);
+            this.btt_2.TabIndex = 13;
+            this.btt_2.Text = "Oyuncu 2 Zar Atsın";
+            this.btt_2.UseVisualStyleBackColor = true;
+            this.btt_2.Click += new System.EventHandler(this.btt_2_Click);
+            // 
+            // btt_1
+            // 
+            this.btt_1.Location = new System.Drawing.Point(27, 221);
+            this.btt_1.Name = "btt_1";
+            this.btt_1.Size = new System.Drawing.Size(128, 35);
+            this.btt_1.TabIndex = 14;
+            this.btt_1.Text = "Oyuncu 1 Zar Atsın";
+            this.btt_1.UseVisualStyleBackColor = true;
+            this.btt_1.Click += new System.EventHandler(this.btt_1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(185, 106);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(128, 109);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 12;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(27, 106);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(128, 109);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(133, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Atılan Zarlar";
+            // 
+            // txt_btt_kctbtsn
+            // 
+            this.txt_btt_kctbtsn.Location = new System.Drawing.Point(136, 18);
+            this.txt_btt_kctbtsn.Name = "txt_btt_kctbtsn";
+            this.txt_btt_kctbtsn.Size = new System.Drawing.Size(100, 20);
+            this.txt_btt_kctbtsn.TabIndex = 10;
+            this.txt_btt_kctbtsn.Text = "5";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Oyun Kaçta Bitsin?";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(332, 412);
+            this.Controls.Add(this.btt_yndn);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lbl_2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lbl_1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btt_2);
+            this.Controls.Add(this.btt_1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_btt_kctbtsn);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btt_yndn;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lbl_2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lbl_1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btt_2;
+        private System.Windows.Forms.Button btt_1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_btt_kctbtsn;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Zar Oyunu/Zar Oyunu/Fr_Zar.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zar_Oyunu
+{
+    public partial class Fr_Zar : Form
+    {
+        public Fr_Zar()
+        {
+            InitializeComponent();
+        }
+
+        Random ransgle = new Random();
+        int oyuncu1toplam, oyuncutoplam = 0;
+        int a, b;
+
+        private void zar_at()
+        {
+            a = ransgle.Next(1, 7);
+            b = ransgle.Next(1, 7);
+            pictureBox1.ImageLocation = "zar\\png" + a.ToString() + ".png";
+            pictureBox2.ImageLocation = "zar\\png" + b.ToString() + ".png";
+
+        }
+
+        private void btt_2_Click(object sender, EventArgs e)
+        {
+            txt_btt_kctbtsn.Enabled = false;
+            pictureBox1.Enabled = true;
+            pictureBox2.Enabled = true;
+            btt_1.Enabled = true;
+            btt_2.Enabled = false;
+            zar_at();
+            oyuncutoplam = oyuncutoplam + a + b;
+            lbl_2.Text = oyuncutoplam.ToString();
+            skor_toplam();
+        }
+
+        private void btt_yndn_Click(object sender, EventArgs e)
+        {
+            txt_btt_kctbtsn.Enabled = true;
+            txt_btt_kctbtsn.Text = "100";
+            btt_1.Enabled = true;
+            btt_2.Enabled = true;
+            label4.Visible = true;
+            pictureBox1.Visible = true;
+            pictureBox2.Visible = true;
+            oyuncu1toplam = 0;
+            oyuncutoplam = 0;
+            lbl_1.Text = "0";
+            lbl_2.Text = "0";
+            btt_yndn.Visible = true;
+        }
+
+        private void skor_toplam()
+        {
+            if (oyuncu1toplam >= Convert.ToInt16(txt_btt_kctbtsn.Text))
+            {
+                label4.Visible = true;
+                label4.Text = "1.Oyuncu Kazandı...";
+                btt_2.Enabled = false;
+                btt_yndn.Visible = true;
+            }
+            if (oyuncutoplam >= Convert.ToInt16(txt_btt_kctbtsn.Text))
+            {
+                label4.Visible = true;
+                label4.Text = "2.Oyuncu Kazandı...";
+                btt_1.Enabled = false;
+                btt_yndn.Visible = true;
+            }
+
+        }
+        private void btt_1_Click(object sender, EventArgs e)
+        {
+            txt_btt_kctbtsn.Enabled = false;
+            pictureBox1.Enabled = true;
+            pictureBox2.Enabled = true;
+            btt_1.Enabled = false;
+            btt_2.Enabled = true;
+            zar_at();
+            oyuncu1toplam = oyuncu1toplam + a + b;
+            lbl_1.Text = oyuncu1toplam.ToString();
+            skor_toplam();
+        }
+    }
+}
